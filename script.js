32a5c232f8979190cce40e7746d5fbe2d27f9c9f
kaidzn <83287458+kaidzn@users.noreply.github.com>
2023-10-31 14:36:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -9,32 +9,41 @@ let score = 20; // Move the score variable outside the event listener function
 
 console.log(randomNumber);
 
-document.querySelector("button").addEventListener('click', function() {
+document.querySelector(".check").addEventListener('click', function() {
     const secretNumber = document.querySelector(".themain-number");
     let inputNumber = Number(document.querySelector("input").value);
     let modeOfGuess = document.querySelector(".mode");
     let scoreValue = document.querySelector("span");
 
     if (randomNumber === inputNumber) {
-        modeOfGuess.textContent = "Congrats!!!";
+        modeOfGuess.textContent = "Жарайсың!!!";
         secretNumber.textContent = randomNumber;
         document.querySelector("body").style.backgroundColor = "#2AAA8A"
         
     } else if (inputNumber > randomNumber) {
         if (score > 1) {
-            modeOfGuess.textContent = "Too High!";
+            modeOfGuess.textContent = "Тым Көп!";
             score--;
             scoreValue.textContent = score;
         } else {
-            modeOfGuess.textContent = "You Lost :("
+            modeOfGuess.textContent = "Жеңілдің :("
         }
     } else if (inputNumber < randomNumber) {
         if (score > 1) {
-            modeOfGuess.textContent = "Too Low!";
+            modeOfGuess.textContent = "Тым аз";
             score--;
             scoreValue.textContent = score;
         } else {
-            modeOfGuess.textContent = "You Lost :("
+            modeOfGuess.textContent = "Жеңілдің :("
         }
     }
 });
+document.querySelector(".reset").addEventListener("click", function(){
+    score = 20;
+    randomNumber = Math.floor(Math.random()*20) + 1;
+    document.querySelector(".mode").textContent = "Болжам басталды...";
+    document.querySelector(".score-p").textContent =  score;
+    document.querySelector(".themain-number").textContent = "?";
+    document.querySelector("input").value = "";
+
+})
